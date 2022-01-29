f9ee74100ae9f52102d84393d37569170674f441
RikuH <45202322+RikuH@users.noreply.github.com>
2022-01-29 12:37:08 +0200

    Animations

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,9 +8,14 @@ public class Player : MonoBehaviour
     //Free moving variables
     public CharacterController controller;
     public float speed = 3f;
+    public float runSpeed;
     public float turnSmoothTime = 0f;
     public Transform cam;
     float turnSmoothVelocity;
+
+    bool ableToMove = false;
+
+    bool isRunning = false;
 
     [SerializeField] GameObject ThirdPersonCamera;
 
@@ -30,17 +35,30 @@ public class Player : MonoBehaviour
 
         anim = GetComponent<Animator>();
 
+        StartCoroutine(PlayerIsStand(2.7f));
+    }
+
+    IEnumerator PlayerIsStand(float gettingUpTime)
+    {
+        controller.enabled = false;
+        yield return new WaitForSeconds(gettingUpTime);
+        controller.enabled = true;
+        ableToMove = true;
     }
 
     // Update is called once per frame
     void Update()
     {
+        ListenTree();
+
+        if (!ableToMove)
+            return;
 
         velocity.y += gravity * Time.deltaTime;
         controller.Move(velocity * Time.deltaTime);
 
         Move();
-        //Animations();
+        Animations();
     }
 
     void Animations()
@@ -51,17 +69,51 @@ public class Player : MonoBehaviour
         currentPos = transform.position;
         if (currentPos != lastPos)
         {
-            anim.SetBool("isRunning", true);
+            anim.SetBool("isWalking", true);
+            anim.SetBool("isRunning", isRunning);
+            
         }
         else
         {
-            anim.SetBool("isRunning", false);
+            anim.SetBool("isWalking", false);
         }
         lastPos = currentPos;
     }
 
+    private void ListenTree()
+    {
+        //Tree is in radius...
+
+
+        //Inputs
+        //if (Input.GetKey(KeyCode.Mouse0))
+        if (Input.GetKey(KeyCode.E))
+        {
+            //Look at the tree
+            //transform.LookAt();
+            anim.SetBool("isListening", true);
+            ableToMove = false;
+        }
+        else
+        {    
+            anim.SetBool("isListening", false);
+            ableToMove = true;
+        }
+    }
+
     private void Move()
     {
+        if (Input.GetKey(KeyCode.LeftShift))
+            isRunning = true;    
+        else
+            isRunning = false;
+
+        if (isRunning == true)
+            runSpeed = speed * 2;
+        else
+            runSpeed = 1;
+        
+
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
         Vector3 direction = new Vector3(horizontal, 0, vertical);
@@ -73,7 +125,7 @@ public class Player : MonoBehaviour
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-            controller.Move(moveDir.normalized * speed * Time.deltaTime);
+            controller.Move(moveDir.normalized * speed * runSpeed * Time.deltaTime);
         }
     }
 }
